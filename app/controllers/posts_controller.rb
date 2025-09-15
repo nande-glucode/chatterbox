f7ad97b867f49a03e74d5234c1ea5592ec9ebe52
C706196 <nande.toyi@vitality.co.uk>
2025-09-15 16:03:45 +0200

    Updating the navigation and responsiveness to window size. Adding the posts functionality and creating a test post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :check_owner, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Post.all.includes(:user).order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+  
+  def check_owner
+    redirect_to posts_path, alert: 'Not authorized' unless @post.user == current_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get "posts/index"
+  get "posts/show"
+  get "posts/new"
+  get "posts/create"
+  get "posts/edit"
+  get "posts/update"
+  get "posts/destroy"
   devise_for :users
+  resources :posts
   root 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
